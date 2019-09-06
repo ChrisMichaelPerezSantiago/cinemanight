8937bc983f16c2c7bd38e4b1d05f5e206d7cee27
Chris Michael <chris.perez@upr.edu>
2019-09-06 10:17:13 -0400

    date added to each latest episodes

--- src/api/scraper/index.js
+++ src/api/scraper/index.js
@@ -4,85 +4,87 @@ const axios = require('axios');
 const URL = require('./url/index.js');
 
 
-const getSeriesVideoContent = async(id = "ver-dark-1x1") =>{
+
+const getSeriesVideoContent = async (id = "ver-dark-1x1") => {
   const res = await fetch(`${URL.EPISODES}/${id}`);
   const body = await res.text();
   const $ = cheerio.load(body);
   const videos = [];
 
 
-  $('#dt_contenedor #contenedor #single #reproductor #playex .play-box-iframe').each((index , element) =>{
+  $('#dt_contenedor #contenedor #single #reproductor #playex .play-box-iframe').each((index, element) => {
     const $element = $(element);
     const iframe_videos = $element.find('iframe.metaframe.rptss').attr('src');
 
     videos.push({
-      iframe:{
+      iframe: {
         option: Number(index + 1),
         video_iframe: iframe_videos
       }
-    });       
-  });  
+    });
+  });
   return await Promise.all(videos);
 };
 
 
-const getMoviesVideoContent = async(id) =>{
+const getMoviesVideoContent = async (id) => {
   const res = await fetch(`${URL.MOVIES_URL}/${id}`);
   const body = await res.text();
   const $ = cheerio.load(body);
   const videos = [];
-  $('#dt_contenedor #contenedor #single #playex .play-box-iframe').each((index , element) =>{
+  $('#dt_contenedor #contenedor #single #playex .play-box-iframe').each((index, element) => {
     const $element = $(element);
     const iframe_videos = $element.find('iframe.metaframe.rptss').attr('src')
     videos.push({
-      iframe:{
+      iframe: {
         option: Number(index + 1),
         video_iframe: iframe_videos
       }
-    });       
-  });    
+    });
+  });
   return await Promise.all(videos);
 };
 
 
-const getLatestEpisodes = async(page) => {
+const getLatestEpisodes = async (page) => {
   const res = await fetch(`${URL.EPISODES}/page/${page}`);
   const body = await res.text();
   const $ = cheerio.load(body);
   const promises = [];
 
-  $('article').each((index , element) =>{
+  $('article').each((index, element) => {
     const $element = $(element);
     const title = $element.find('.poster img').attr('alt');
     const episode_name = $element.find('div.data h3').text();
     const poster = $element.find('.poster img').attr('src');
     const quality = $element.find('.poster span.quality').text();
+    const date = $element.find('.data span').text();
     const id = $element.find('div div.season_m.animation-1 a').attr('href').split('/')[4]
     promises.push({
       id: id || 'unknown',
       title: title || 'unknown',
       episode_name: episode_name || 'unknown',
       poster: poster || 'unknown',
+      date: date || 'unknown',
       quality: quality || 'unknown',
     })
-  });  
+  });
   console.log(promises);
   return await Promise.all(promises);
 };
-
 
 
 /**
  * @param {*} genres = [accion , aventura , comedia , crimen , drama , familia , fantasia , misterio , suspense , suspenso , terror]
  * @param {*} page = unknown limit
  */
-const getByGenres = async(genre , page) =>{
+const getByGenres = async (genre, page) => {
   const res = await fetch(`${URL.GENRES}/${genre}/page/${page}`);
   const body = await res.text();
   const $ = cheerio.load(body);
   const promises = [];
 
-  $('article').each((index , element) =>{
+  $('article').each((index, element) => {
     const $element = $(element);
     const title = $element.find('.animation-1 .title h4').text();
     const sinopsis = $element.find('.animation-1 .texto').text();
@@ -92,8 +94,8 @@ const getByGenres = async(genre , page) =>{
     const year = $element.find('.data span').text();
     const id = $element.find('.data h3 a').attr('href').split('/')[4];
     const type = $element.find('.data h3 a').attr('href').split('/')[3];
-    if(type === 'serie'){
-      promises.push(seriesHandler(id).then(async(extra) => ({
+    if (type === 'serie') {
+      promises.push(seriesHandler(id).then(async (extra) => ({
         id: id || 'unknown',
         title: title || 'unknown',
         poster: poster || 'unknown',
@@ -105,8 +107,8 @@ const getByGenres = async(genre , page) =>{
         extra: extra
       })))
     }
-    if(type === 'pelicula'){
-      promises.push(movieHandler(id).then(async(extra) => ({
+    if (type === 'pelicula') {
+      promises.push(movieHandler(id).then(async (extra) => ({
         id: id || 'unknown',
         title: title || 'unknown',
         poster: poster || 'unknown',
@@ -123,23 +125,23 @@ const getByGenres = async(genre , page) =>{
 }
 
 
-const search = async(title) =>{
+const search = async (title) => {
   const res = await fetch(`${URL.SEARCH_URL}${title}`);
   const body = await res.text();
   const $ = cheerio.load(body);
   const promises = [];
 
   $('#dt_contenedor #contenedor .module .content.csearch .search-page .result-item article')
-    .each(async(index, element) =>{
+    .each(async (index, element) => {
       const $element = $(element);
       const title = $element.find('.details .title').text().trim();
       const poster = $element.find('.image .thumbnail.animation-2 a img').attr('src');
       const synopsis = $element.find('.details .contenido p').text();
       const type = $element.find('.details .title a').attr('href').split('/')[3];
       const id = $element.find('.details .title a').attr('href').split('/')[4];
-      
-      if(type === 'serie'){
-        promises.push(seriesHandler(id).then(async(extra) => ({
+
+      if (type === 'serie') {
+        promises.push(seriesHandler(id).then(async (extra) => ({
           id: id || 'unknown',
           title: title || 'unknown',
           poster: poster || 'unknown',
@@ -148,8 +150,8 @@ const search = async(title) =>{
           extra: extra
         })))
       }
-      if(type === 'pelicula'){
-        promises.push(movieHandler(id).then(async(extra) => ({
+      if (type === 'pelicula') {
+        promises.push(movieHandler(id).then(async (extra) => ({
           id: id || 'unknown',
           title: title || 'unknown',
           poster: poster || 'unknown',
@@ -159,17 +161,17 @@ const search = async(title) =>{
         })))
       }
     });
-    return await Promise.all(promises);
+  return await Promise.all(promises);
 };
 
 
-const getAllMovies = async(page = 1) =>{
+const getAllMovies = async (page = 1) => {
   const res = await fetch(`${URL.MOVIES_URL}/page/${page}`);
   const body = await res.text();
   const promises = [];
   const $ = cheerio.load(body);
 
-  $('article').each((index , element) =>{
+  $('article').each((index, element) => {
     const $element = $(element);
     const title = $element.find('.animation-1 .title h4').text();
     const sinopsis = $element.find('.animation-1 .texto').text();
@@ -192,22 +194,22 @@ const getAllMovies = async(page = 1) =>{
   return await Promise.all(promises);
 }
 
-const movieHandler = async(id) =>{
+const movieHandler = async (id) => {
   const res = await fetch(`${URL.MOVIES_URL}/${id}`);
   const body = await res.text();
   const promises = [];
   const $ = cheerio.load(body);
 
-  $('#dt_contenedor').each((index , element) =>{
+  $('#dt_contenedor').each((index, element) => {
     const $element = $(element);
     const air_date = $element.find('#single div.sheader div.data div.extra span.date').text();
     const country = $element.find('#single div.sheader div.data div.extra span.country').text();
     const rated = $element.find('#single div.sheader div.data div.extra span').eq(3).text();
-    const runtime = $element.find('#single div.sheader div.data div.extra span.runtime').text().replace('.' , '').trim();
+    const runtime = $element.find('#single div.sheader div.data div.extra span.runtime').text().replace('.', '').trim();
 
     const members_list = []
     const members_info = []
-    $element.find('#single.dtsingle .content #cast.sbox.fixidtab div.persons div.person').each((j , content) =>{
+    $element.find('#single.dtsingle .content #cast.sbox.fixidtab div.persons div.person').each((j, content) => {
       members_info.push({
         characters: {
           real_name: $element.find('div.img a img').eq(Number(j + 1)).attr('alt') || 'unknown',
@@ -217,16 +219,18 @@ const movieHandler = async(id) =>{
       })
     });
 
-    members_list.push({members_info})
+    members_list.push({
+      members_info
+    })
 
-      
+
     const cast_members = {
-      creator:{
+      creator: {
         name: $element.find('#single.dtsingle .content #cast.sbox.fixidtab div.persons div.person div.img a img').eq(0).attr('alt') || 'unknown',
         poster: $element.find('#single.dtsingle .content #cast.sbox.fixidtab div.persons div.person div.img a img').eq(0).attr('src') || 'unknown',
       },
       members_list: members_list
-    }  
+    }
     promises.push({
       air_date: air_date || 'unknown',
       country: country || 'unknown',
@@ -235,18 +239,18 @@ const movieHandler = async(id) =>{
       cast_members: cast_members
     });
   });
-  
+
   return await Promise.all(promises);
 };
 
 
-const getAllSeries = async(page = 1) => {
+const getAllSeries = async (page = 1) => {
   const res = await fetch(`${URL.SERIES_URL}/page/${page}`);
   const body = await res.text();
   const promises = [];
   const $ = cheerio.load(body);
 
-  $('article').each((index , element) =>{
+  $('article').each((index, element) => {
     const $element = $(element);
     const title = $element.find('.animation-1 .title h4').text();
     const sinopsis = $element.find('.animation-1 .texto').text();
@@ -263,42 +267,44 @@ const getAllSeries = async(page = 1) => {
       year: year || 'unknown',
       extra: extra || 'unknown'
     })))
-  });  
+  });
   return await Promise.all(promises);
 };
 
 
-const seriesHandler = async(id) =>{
+const seriesHandler = async (id) => {
   const res = await fetch(`${URL.SERIES_URL}/${id}`);
   const body = await res.text();
   const promises = [];
   const $ = cheerio.load(body);
 
-  $('#dt_contenedor').each((index , element) =>{
+  $('#dt_contenedor').each((index, element) => {
     const $element = $(element);
     const channel = $element.find('#single div.sheader div.data div.extra span a').text();
     const first_air_date = $element.find('#info .custom_fields').eq(1).text();
     const last_air_date = $element.find('#info .custom_fields').eq(2).text();
-    const total_seasons = $element.find('#info .custom_fields').eq(3).text().replace(/[^0-9]/g , '');
-    const total_episodes = $element.find('#info .custom_fields').eq(4).text().replace(/[^0-9]/g , '');
+    const total_seasons = $element.find('#info .custom_fields').eq(3).text().replace(/[^0-9]/g, '');
+    const total_episodes = $element.find('#info .custom_fields').eq(4).text().replace(/[^0-9]/g, '');
     const season_list = []
-    $('#episodes #serie_contenido #seasons').each((index , el) =>{
+    $('#episodes #serie_contenido #seasons').each((index, el) => {
       const $el = $(el);
-      $el.find('div.se-a ul.episodios').each((i , e) =>{
+      $el.find('div.se-a ul.episodios').each((i, e) => {
         const $e = $(e);
         const season = Number(i + 1) || 'unknown';
         const episodes = $e.find('div.numerando').text().split('-').slice(-1).join().trim() || 'unknown'
         const extra = {
-          season: season, 
-          episodes: Array.from({length: episodes}, (v , k) => k + 1).map(num => `${season}x${num}`)
+          season: season,
+          episodes: Array.from({
+            length: episodes
+          }, (v, k) => k + 1).map(num => `${season}x${num}`)
         };
         season_list.push(extra);
       })
     })
-    
+
     const members_list = []
     const members_info = []
-    $element.find('#single.dtsingle .content #cast.sbox.fixidtab div.persons div.person').each((j , content) =>{
+    $element.find('#single.dtsingle .content #cast.sbox.fixidtab div.persons div.person').each((j, content) => {
       members_info.push({
         characters: {
           real_name: $element.find('div.img a img').eq(Number(j + 1)).attr('alt') || 'unknown',
@@ -308,18 +314,20 @@ const seriesHandler = async(id) =>{
       })
     });
 
-    members_list.push({members_info})
+    members_list.push({
+      members_info
+    })
 
-      
+
     const cast_members = {
-      creator:{
+      creator: {
         name: $element.find('#single.dtsingle .content #cast.sbox.fixidtab div.persons div.person div.img a img').eq(0).attr('alt') || 'unknown',
         poster: $element.find('#single.dtsingle .content #cast.sbox.fixidtab div.persons div.person div.img a img').eq(0).attr('src') || 'unknown',
       },
       members_list: members_list
-    }  
+    }
     promises.push({
-      channel: channel || 'unknown',                  
+      channel: channel || 'unknown',
       first_air_date: first_air_date || 'unknown',
       last_air_date: last_air_date || 'unknown',
       total_seasons: total_seasons || 'unknown',
@@ -327,7 +335,7 @@ const seriesHandler = async(id) =>{
       season_list: season_list || 'unknown',
       cast_members: cast_members
     })
-  });  
+  });
   return await Promise.all(promises);
 };
 

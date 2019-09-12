052ac52554c622f1d6ec8a04d94ef09f5a50fa9c
Chris Michael <chris.perez@upr.edu>
2019-09-11 22:04:47 -0400

    one header added and package version 1+

--- src/app.js
+++ src/app.js
@@ -20,8 +20,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 
 app.use(function(req , res , next){
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin: *');
+  res.header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
   next();
 });
 
